0528ab6a8b203ff6c6e57ffc4e9d3f9ee1dbf35b
sixthsurge <surgecrafter@gmail.com>
2026-08-05 15:17:34 +0100

    Add atmosphere

--- src/java/Fracture.java
+++ src/java/Fracture.java
@@ -21,7 +21,11 @@ public class Fracture implements ShaderPack {
 		}
 	}
 
-	public record GlobalBufferData(Vector3f light_dir_world) {}
+	public record GlobalBufferData(
+		Vector3f light_dir_world,
+		Vector3f sun_dir_world,
+		Vector3f moon_dir_world
+	) {}
 
 	MappedBuffer<GlobalBufferData> globalBuffer;
 
@@ -49,6 +53,33 @@ public class Fracture implements ShaderPack {
 				  .renderSize()
 				  .create();
 
+		final var atmosphereTransmittanceTex
+			= pipeline
+				  .texture2D(
+					  "tex_atmosphere_transmittance_lut",
+					  TextureFormat.RG11B10_UFLOAT
+				  )
+				  .size(256, 64)
+				  .create();
+
+		final var atmosphereMultiscatterTex
+			= pipeline
+				  .texture2D(
+					  "tex_atmosphere_multiscatter_lut",
+					  TextureFormat.RGBA16_SFLOAT
+				  )
+				  .size(32, 32)
+				  .create();
+
+		final var atmosphereSkyViewTex
+			= pipeline
+				  .texture2D(
+					  "tex_atmosphere_sky_view",
+					  TextureFormat.RG11B10_UFLOAT
+				  )
+				  .size(512, 256)
+				  .create();
+
 		globalBuffer
 			= pipeline.mappedBuffer("buf_global", GlobalBufferData.class);
 
@@ -64,12 +95,38 @@ public class Fracture implements ShaderPack {
 			);
 		}
 
+		pipeline.stage(ProgramStage.PRE_RENDER)
+			.composite(
+				"atmosphere/transmittance_lut",
+				"program/atmosphere",
+				"transmittance_lut_main"
+			)
+			.writes("transmittance", atmosphereTransmittanceTex);
+
+		pipeline.stage(ProgramStage.PRE_RENDER)
+			.composite(
+				"atmosphere/multiscatter_lut",
+				"program/atmosphere",
+				"multiscatter_lut_main"
+			)
+			.writes("multiscatter_energy", atmosphereMultiscatterTex);
+
+		pipeline.stage(ProgramStage.PRE_RENDER)
+			.composite(
+				"atmosphere/sky_view",
+				"program/atmosphere",
+				"sky_view_main"
+			)
+			.writes("sky_radiance", atmosphereSkyViewTex);
+
 		pipeline.stage(ProgramStage.PRE_TRANSLUCENT)
 			.composite("deferred_shading", "program/deferred_shading", "main")
 			.writes("radiance", sceneTex.back());
 
-		// Must have scene data in front and back for translucent passes to read and write sceneTex.
-		pipeline.stage(ProgramStage.PRE_TRANSLUCENT).copy(sceneTex.back(), sceneTex.front());
+		// Must have scene data in front and back for translucent passes to read
+		// and write sceneTex.
+		pipeline.stage(ProgramStage.PRE_TRANSLUCENT)
+			.copy(sceneTex.back(), sceneTex.front());
 
 		pipeline
 			.object(
@@ -77,10 +134,12 @@ public class Fracture implements ShaderPack {
 				"program/object/translucent",
 				"TranslucentObject"
 			)
-			.writes("color", sceneTex.back()).overrideObject("tex_scene", sceneTex.front().name());
+			.writes("color", sceneTex.back())
+			.overrideObject("tex_scene", sceneTex.front().name());
 		sceneTex.flip();
 
-		pipeline.combinationPass("program/combination").overrideObject("tex_scene", sceneTex.front().name());
+		pipeline.combinationPass("program/combination")
+			.overrideObject("tex_scene", sceneTex.front().name());
 	}
 
 	@Override
@@ -96,7 +155,12 @@ public class Fracture implements ShaderPack {
 	@Override
 	public void onNewFrame(FrameState state) {
 		globalBuffer.write(new GlobalBufferData(
-			state.uniforms().getFloat3("ap.celestial.position").normalize()
+			state.uniforms().getFloat3("ap.celestial.position").normalize(),
+			state.uniforms().getFloat3("ap.celestial.sunPosition").normalize(),
+			state.uniforms()
+				.getFloat3("ap.celestial.sunPosition")
+				.negate()
+				.normalize()
 		));
 	}
 }
